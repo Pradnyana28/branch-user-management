70612b3d1ba54ea2d2aed90220acf22d3dfe38e1
Kadek Pradnyana <ikadek.pradnyana@mitrais.com>
2021-08-23 20:45:40 +0800

    update mongodb uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,7 @@ import { UserModule } from './user/user.module';
   imports: [
     ConfigModule.forRoot(),
     UserModule,
-    MongooseModule.forRoot(
-      `mongodb://${process.env.MONGO_INITDB_USERNAME}:${process.env.MONGO_INITDB_PASSWORD}@${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGO_INITDB_DATABASE}`,
-    ),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [],
   providers: [],
